5befe76c57836d54dc3d945f37adf73e64c04f74
Pedro Henrique <p13dr0h@gmail.com>
2019-07-10 01:32:18 -0300

    first

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('distancia-form.html')
+
+
+@app.route('/calcularDistancia/', methods=['POST'])
+def calcular_distancia():
+    distancia = int(request.form['distancia'])
+    return render_template('resultado-distancia.html', distancia=distancia)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
